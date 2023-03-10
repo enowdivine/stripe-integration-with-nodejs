687be94190ae3e6cfad1acf31d26a3dde7af29ca
Enow Divine <sirdivine16@gmail.com>
2023-03-10 14:52:58 +0100

    stripe integration with nodejs

---
+++ backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image: "/images/iphone12pro.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image: "/images/iphone12.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image: "/images/galaxyS.png",
+  },
+];
+
+module.exports = products;
